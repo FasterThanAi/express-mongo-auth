767c41001e0208ddc084ce2d505bb30d331707f3
FasterThanAi <demeanor404@gmail.com>
2026-03-20 16:51:38 +0530

    Add authentication routes to the Express server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import 'dotenv/config';
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/authRoutes.js"
 const app=express();
 const port=process.env.PORT|| 3000
 
@@ -11,8 +12,9 @@ app.use(cors({credentials:true}))
 app.use(cookieParser())
 
 connectDB();
-
+// API Endpoints
 app.get('/',(req,res)=>res.send("API working fine"))
+app.use('/api/auth',authRouter)
 
 app.listen(port,()=>{
     console.log(`Server started on port ${port}`)
