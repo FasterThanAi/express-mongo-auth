6b01878d739306f4ab04e030394baf57cb8ad683
FasterThanAi <demeanor404@gmail.com>
2026-03-20 16:51:53 +0530

    Add authentication routes for user registration, login, and logout

---
+++ routes/authRoutes.js
@@ -0,0 +1,8 @@
+import express from 'express'
+import { login, logout, register } from '../controllers/authController.js';
+
+const authRouter= express.Router();
+
+authRouter.post('/register',register ) // controller func register
+authRouter.post('/login',login ) // controller func login
+authRouter.post('/login',logout ) // controller func logout
